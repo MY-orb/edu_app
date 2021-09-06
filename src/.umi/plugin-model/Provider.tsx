3eba43243931b163d569ae38a16c5b5089c727ed
wangyidong <wangyidong@innobuddy.com>
2021-09-06 10:16:06 +0800

    feat: 部分更新

--- src/.umi/plugin-model/Provider.tsx
+++ src/.umi/plugin-model/Provider.tsx
@@ -1,13 +1,13 @@
 // @ts-nocheck
 import React from 'react';
-import initialState from '/Users/wtqfolder/Desktop/study/edu/edu_app/src/.umi/plugin-initial-state/models/initialState';
+import initialState from '/Users/dd-ream/Desktop/wyd_work/edu_app/src/.umi/plugin-initial-state/models/initialState';
 
 // @ts-ignore
-import Dispatcher from './helpers/dispatcher';
+import Dispatcher from '/Users/dd-ream/Desktop/wyd_work/edu_app/node_modules/@umijs/plugin-model/lib/helpers/dispatcher';
 // @ts-ignore
-import Executor from './helpers/executor';
+import Executor from '/Users/dd-ream/Desktop/wyd_work/edu_app/node_modules/@umijs/plugin-model/lib/helpers/executor';
 // @ts-ignore
-import { UmiContext } from './helpers/constant';
+import { UmiContext } from '/Users/dd-ream/Desktop/wyd_work/edu_app/node_modules/@umijs/plugin-model/lib/helpers/constant';
 
 export const models = { '@@initialState': initialState,  };
 
--- src/.umi/umi.ts
+++ src/.umi/umi.ts
@@ -4,8 +4,8 @@ import '@@/core/devScripts';
 import { plugin } from './core/plugin';
 import './core/pluginRegister';
 import { createHistory } from './core/history';
-import { ApplyPluginsType } from '/Users/wtqfolder/Desktop/study/edu/edu_app/node_modules/umi/node_modules/@umijs/runtime';
-import { renderClient } from '/Users/wtqfolder/Desktop/study/edu/edu_app/node_modules/umi/node_modules/@umijs/renderer-react/dist/index.js';
+import { ApplyPluginsType } from '/Users/dd-ream/Desktop/wyd_work/edu_app/node_modules/umi/node_modules/@umijs/runtime';
+import { renderClient } from '/Users/dd-ream/Desktop/wyd_work/edu_app/node_modules/umi/node_modules/@umijs/renderer-react/dist/index.js';
 import { getRoutes } from './core/routes';
 
 
@@ -37,7 +37,7 @@ export default clientRender();
 
 
     window.g_umi = {
-      version: '3.4.22',
+      version: '3.4.11',
     };
   
 
